9c5968050db783c5ffc03cee8d14b36ff376c128
AnchalaSnehalatha <146068603+anchalasnehalatha@users.noreply.github.com>
2024-11-17 15:06:00 +0530

    Create countfactors.java

---
+++ countfactors.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number: ");
+        int n = sc.nextInt();
+        int res = factors(n);
+        System.out.println("Number of factors: " + res);
+    }
+
+    static int factors(int n) {
+        int count = 1;
+        for (int i = 1; i <= n; i++) {
+            if (n % i == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+
+Count factors
